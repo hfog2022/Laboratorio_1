d55dfc8a2b57af2812a3e1db1686f0220be5bc32
20661 <2066@holbertonschool.com>
2022-07-11 19:21:50 -0500

    Problema 11 files

---
+++ Problema_11/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * Date: 22/06/2022
+ * Author: Hector Orozco
+ * Informatica II 2022-1
+ * Escribe un programa que reciba un número
+ * y calcula el mínimo común múltiplo de
+ * todos los números enteros entre 1 y el
+ * numero ingresado.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int numero;
+    cout << "Por favor ingrese un numero: ";
+    cin >> numero;
+
+
+
+
+    return 0;
+}
